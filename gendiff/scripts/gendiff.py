1a2990187bd4453e02c7db0e7bdc9f4ca59b3489
i-pichurov <deadbassein@gmail.com>
2024-09-29 20:04:13 +0300

    add -f option to gendiff.py

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -5,4 +5,5 @@ import argparse
 parser = argparse.ArgumentParser(description='Compares two configuration files and shows a difference.')
 parser.add_argument('first_file', type=str, help='Input first file for comparison')
 parser.add_argument('second_file', type=str, help='Input second file for comparison')
+parser.add_argument('-f', '--format', type=str, help='set format of output')
 args = parser.parse_args()
