be1f8a4d88e97b84a5eb3e6df20a7b2a236dd97f
i-pichurov <deadbassein@gmail.com>
2024-09-29 19:46:52 +0300

    add gendiff/scripts/gendiff.py

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
-def main():
-    print("Hello!")
+import argparse
 
-if __name__ == '__main__':
-    main()
+
+parser = argparse.ArgumentParser(description='Compares two configuration files and shows a difference.')
+parser.add_argument('first_file', type=str, help='Input first file for comparison')
+parser.add_argument('second_file', type=str, help='Input second file for comparison')
+args = parser.parse_args()
