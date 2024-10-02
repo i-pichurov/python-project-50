723796795124267ccdf7b7165f277413436ea06a
i-pichurov <deadbassein@gmail.com>
2024-10-02 13:06:36 +0300

    prepare tests for step6

--- tests/test_generate_diff.py
+++ tests/test_generate_diff.py
@@ -5,6 +5,10 @@ from gendiff import generate_diff
 with open('tests/fixtures/result.txt') as r:
         result = r.read()
 
-# Тестриуем результат функции с образцовой строкой
+
 def test_generate_diff():
+    # Тестриуем результат функции с json-файлами и образцовой строкой
     assert generate_diff('tests/fixtures/file1.json', 'tests/fixtures/file2.json') == result
+
+    # Тестриуем результат функции с yml-файлами и образцовой строкой
+    assert generate_diff('tests/fixtures/filepath1.yml', 'tests/fixtures/filepath2.yml') == result
