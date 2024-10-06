db8bbc1c267de67f4f46a9d36e8edb3de1cca31a
i-pichurov <deadbassein@gmail.com>
2024-10-06 22:17:18 +0300

    prepare tests for step7

--- tests/test_generate_diff.py
+++ tests/test_generate_diff.py
@@ -1,14 +1,26 @@
-from gendiff import generate_diff
+from gendiff import engine
 
 
-# Извлекаем образцовую строку
+# Извлекаем плоскую образцовую строку
 with open('tests/fixtures/result.txt') as r:
         result = r.read()
 
+# Извлекаем многоуровневую образцовую строку
+with open('tests/fixtures/result_rec.txt') as r:
+        result_rec = r.read()
 
-def test_generate_diff():
+
+def test_engine():
     # Тестриуем результат функции с json-файлами и образцовой строкой
-    assert generate_diff('tests/fixtures/file1.json', 'tests/fixtures/file2.json') == result
+    assert engine('tests/fixtures/file1.json', 'tests/fixtures/file2.json') == result
 
     # Тестриуем результат функции с yml-файлами и образцовой строкой
-    assert generate_diff('tests/fixtures/filepath1.yml', 'tests/fixtures/filepath2.yml') == result
+    assert engine('tests/fixtures/filepath1.yml', 'tests/fixtures/filepath2.yml') == result
+
+
+def test_engine():
+    # Тестриуем результат функции с json-файлами и образцовой строкой
+    assert engine('tests/fixtures/file1_rec.json', 'tests/fixtures/file2_rec.json') == result_rec
+
+    # Тестриуем результат функции с yml-файлами и образцовой строкой
+    assert engine('tests/fixtures/file1_rec.yml', 'tests/fixtures/file2_rec.yml') == result_rec
