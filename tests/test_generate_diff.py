e75b09a3565d49db388dfdd92ae74487096e95eb
i-pichurov <deadbassein@gmail.com>
2024-10-08 21:05:06 +0300

    prepare tests for step8

--- tests/test_generate_diff.py
+++ tests/test_generate_diff.py
@@ -5,20 +5,24 @@ from gendiff import engine
 with open('tests/fixtures/result.txt') as r:
         result = r.read()
 
-# Извлекаем многоуровневую образцовую строку
-with open('tests/fixtures/result_rec.txt') as r:
-        result_rec = r.read()
+# Извлекаем многоуровневую образцовую stylish-строку
+with open('tests/fixtures/result_stylish.txt') as r:
+        result_stylish = r.read()
 
+# Извлекаем многоуровневую образцовую stylish-строку
+with open('tests/fixtures/result_plain.txt') as r:
+        result_plain = r.read()
 
 def test_engine():
-    # Тестриуем результат функции с json-файлами и образцовой строкой
+    # Тестриуем результат функции с плоскими json/yml -файлами
     assert engine('tests/fixtures/file1.json', 'tests/fixtures/file2.json') == result
-
-    # Тестриуем результат функции с yml-файлами и образцовой строкой
     assert engine('tests/fixtures/filepath1.yml', 'tests/fixtures/filepath2.yml') == result
 
-    # Тестриуем результат функции с json-файлами и образцовой строкой
-    assert engine('tests/fixtures/file1_rec.json', 'tests/fixtures/file2_rec.json') == result_rec
+    # Тестриуем результат функции с вложенными json/yml -файлами в режиме stylish
+    assert engine('tests/fixtures/file1_rec.json', 'tests/fixtures/file2_rec.json', 'stylish') == result_stylish
+    assert engine('tests/fixtures/file1_rec.yml', 'tests/fixtures/file2_rec.yml', 'stylish') == result_stylish
 
-    # Тестриуем результат функции с yml-файлами и образцовой строкой
-    assert engine('tests/fixtures/file1_rec.yml', 'tests/fixtures/file2_rec.yml') == result_rec
+    # Тестриуем результат функции с вложенными json/yml -файлами в режиме plain
+    assert engine('tests/fixtures/file1_rec.json', 'tests/fixtures/file2_rec.json', 'plain') == result_plain
+    assert engine('tests/fixtures/file1_rec.json', 'tests/fixtures/file2_rec.json', 'plain') == result_plain
+
