c3f2587576db9bee457397b2e6886bf9ecb9f0cf
i-pichurov <deadbassein@gmail.com>
2024-10-16 23:05:34 +0300

    1st check: p.5, 6

--- tests/test_generate_diff.py
+++ tests/test_generate_diff.py
@@ -1,38 +1,72 @@
 from gendiff import generate_diff
-import json
 import pytest
 
 
-# Извлекаем образцовую stylish-строку
-with open('tests/fixtures/result_stylish.txt') as r:
-    result_stylish = r.read()
+@pytest.mark.parametrize(
+    "first_file, second_file, format_name, result_fixture",
+    [
+        (
+            'tests/fixtures/file1.json',
+            'tests/fixtures/file2.json',
+            'stylish',
+            'tests/fixtures/result_stylish.txt'
+        ),
+        (
+            'tests/fixtures/file1.yml',
+            'tests/fixtures/file2.yml',
+            'stylish',
+            'tests/fixtures/result_stylish.txt'
+        ),
+        (
+            'tests/fixtures/file1.json',
+            'tests/fixtures/file2.yml',
+            'stylish',
+            'tests/fixtures/result_stylish.txt'
+        ),
+        (
+            'tests/fixtures/file1.json',
+            'tests/fixtures/file2.json',
+            'plain',
+            'tests/fixtures/result_plain.txt'
+        ),
+        (
+            'tests/fixtures/file1.yml',
+            'tests/fixtures/file2.yml',
+            'plain',
+            'tests/fixtures/result_plain.txt'
+        ),
+        (
+            'tests/fixtures/file1.json',
+            'tests/fixtures/file2.yml',
+            'plain',
+            'tests/fixtures/result_plain.txt'
+        ),
+        (
+            'tests/fixtures/file1.json',
+            'tests/fixtures/file2.json',
+            'json',
+            'tests/fixtures/result_json.txt'
+        ),
+        (
+            'tests/fixtures/file1.yml',
+            'tests/fixtures/file2.yml',
+            'json',
+            'tests/fixtures/result_json.txt'
+        ),
+        (
+            'tests/fixtures/file1.json',
+            'tests/fixtures/file2.yml',
+            'json',
+            'tests/fixtures/result_json.txt'
+        ),
+    ])
+def test_generate_diff(first_file, second_file, format_name, result_fixture):
+    with open (result_fixture) as r:
+        result = r.read()
+    assert generate_diff(first_file, second_file, format_name) == result
 
-# Извлекаем образцовую plain-строку
-with open('tests/fixtures/result_plain.txt') as r:
-    result_plain = r.read()
-
-# Извлекаем образцовый json
-with open('tests/fixtures/result_json.json', 'r') as r:
-    result_json = json.load(r)
-
-
-def test_generate_diff():
-    # Тестриуем результат функции с вложенными json/yml -файлами в режиме stylish
-    assert generate_diff('tests/fixtures/file1.json', 'tests/fixtures/file2.json', 'stylish') == result_stylish
-    assert generate_diff('tests/fixtures/file1.yml', 'tests/fixtures/file2.yml', 'stylish') == result_stylish
-
-    # Тестриуем результат функции с вложенными json/yml -файлами в режиме plain
-    assert generate_diff('tests/fixtures/file1.json', 'tests/fixtures/file2.json', 'plain') == result_plain
-    assert generate_diff('tests/fixtures/file1.json', 'tests/fixtures/file2.json', 'plain') == result_plain
-
-    # Тестриуем результат функции с вложенными json/yml -файлами в режиме json
-    assert json.loads(generate_diff('tests/fixtures/file1.json', 'tests/fixtures/file2.json', 'json')) == result_json
-    assert json.loads(generate_diff('tests/fixtures/file1.json', 'tests/fixtures/file2.json', 'json')) == result_json
-
-    # Тестируем выпадение ошибки при некорректном format_name
     with pytest.raises(Exception):
         generate_diff('tests/fixtures/file1.json', 'tests/fixtures/file2.json', 'spanish')
 
-    # Тестируем выпадение ошибки при некорректном file_format
     with pytest.raises(Exception):
         generate_diff('tests/fixtures/file1.txt', 'tests/fixtures/file2.txt')
